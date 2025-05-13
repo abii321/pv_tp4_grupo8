4e9923e3902e2c0be9bad48288fc0ade0baca9a4
lujancansino <celestelujancansino@gmail.com>
2025-05-12 23:58:45 -0300

    searchbyid en app.jsx-Lujan Cansino

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import ProductForm from './assets/components/ProductForm.jsx';
 import ProductList from './assets/components/ProductList.jsx';
 import SearchBar from './assets/components/SearchBar.jsx';
-import SearchById from './assets/components/SearchById.jsx';
+import SearchById from './assets/components/SearchById.jsx';  
 
 function App() {
   const [productos, setProductos] = useState([]); 
@@ -11,8 +11,8 @@ function App() {
     <>
       <ProductForm productos={productos} setProductos={setProductos} />
       <ProductList productos={productos} />
-      {/**<SearchById productos={productos} /> 
-      <ProductList productos={productos} />  
+      <SearchById productos={productos} /> 
+     {/** <ProductList productos={productos} />  
       <SearchBar productos={productos} /> **/}
     </>
   );
